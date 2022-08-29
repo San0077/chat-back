7f5e7ce276bb58dfbbe048aade18609ad99198bd
Santhosh <99713211+San0077@users.noreply.github.com>
2022-08-29 13:00:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ import express from "express"
 import cors from "cors"
 import http from "http"
 import dotenv from 'dotenv'
+import { createServer } from "http";
 
 const app = express()
 app.use(cors())
-const server = http.createServer(app)
+const server = createServer(app)
 dotenv.config()
-const io  = new Server(server)
-var PORT = process.env.PORT
+const io  = new Server(server,{
+    cors: {
+        origin: ["http://localhost:3000","https://charming-sprinkles-819e65.netlify.app/"],
+        methods: ["GET", "POST"],
+      },
+})
+var port = process.env.PORT
 
 
-io.on("connection",(socket)=>{
+io.of("/").on("connection",(socket)=>{
    socket.on("join_room",(data)=>{
       socket.join(data)
-       console.log(socket.id)
+       console.log(data)
       
       
    })
@@ -30,6 +36,6 @@ io.on("connection",(socket)=>{
 })
 
 
-server.listen(PORT,()=>{
-    console.log("server is ready")
+server.listen(port,()=>{
+    console.log("server is ready"+port)
 });
