958e1f407d2f40c452e0838f568455ca52f17272
Santhosh <99713211+San0077@users.noreply.github.com>
2022-08-29 13:48:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = createServer(app)
 dotenv.config()
 const io  = new Server(server,{
     cors: {
-        origin: "http://localhost:3000/",
+        origin: ["http://localhost:3000","https://charming-sprinkles-819e65.netlify.app/"],
         methods: ["GET", "POST"],
       },
 })
